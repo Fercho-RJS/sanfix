0c783bca730bf0bb5f9b22d5c2b543e629d5a6de
David Rojas <fernandorojas.contacto@gmail.com>
2025-09-28 14:54:54 -0300

    Avanzado

---
+++ pages/proveedores.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es" data-bs-theme="">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Inicio</title>
+
+  <!-- Estilos compartidos -->
+  <?php require_once __DIR__ . '/pages/shared/styles.php'; ?>
+
+  <!-- Estilos específicos -->
+  <link rel="stylesheet" href="sanfix/style/custom-index.css">
+</head>
+
+<body class="bg-dark">
+  <?php require_once __DIR__ . '/pages/shared/header.php'; ?>
+
+  <main class="container bg-body my-3 p-4 rounded-4"> <!-- Inicio del ontenido de ésta web -->
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui cumque odit temporibus repellat voluptas unde itaque ducimus voluptatum alias fugiat laboriosam est, id culpa reiciendis exercitationem illum soluta? Hic, veritatis.
+    Cum optio quas quae maxime placeat, laborum repellendus odit nisi nobis beatae tempore quisquam minus et impedit, fugiat ut omnis sequi totam debitis excepturi fugit. Rerum officiis adipisci nesciunt ad.
+    Deleniti aliquam architecto consectetur tempora mollitia minus illum ullam aspernatur provident reprehenderit. Dolore natus laudantium esse architecto cupiditate neque, vero perspiciatis nesciunt reprehenderit necessitatibus! Quam similique facere consequatur. Nostrum, nihil.
+    Nobis suscipit sint obcaecati quidem quae, esse sapiente repellat voluptas similique. Maxime, quisquam. Aut laborum earum accusamus eos, aspernatur beatae voluptatum, placeat suscipit vel tempora ex. Facere quasi dicta reprehenderit.</p>
+  </main> <!-- Fin del contenido de ésta web --
+
+  <?php require_once __DIR__ . '/pages/shared/footer.php'; ?>
+</body>
+
+<!-- Scripts compartidos -->
+  <?php require_once __DIR__ . '/pages/shared/scripts.php'; ?>
+
+  <!-- Scripts específicos -->
+  <script src="sanfix/scripts/custom-index.js"></script>
+
+</html>
